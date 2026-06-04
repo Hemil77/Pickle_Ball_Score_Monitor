b073215ea43422fac079ee53185c9c4c791369fb
Hemil Patel <167183225+Hemil77@users.noreply.github.com>
2026-06-04 14:39:59 +0530

    normal url changes
    Removed unused URL patterns from urlpatterns.

--- score/urls.py
+++ score/urls.py
@@ -3,10 +3,5 @@ from . import views
 
 urlpatterns =[
     path("", views.vatsal),
-    path("home", views.energy),
-    path("mullo", views.om),
-    path("pickleball", views.pickleball),
-    path("energy", views.energy),
-    path("vatsal", views.vatsal),
-    
+    path("home", views.energy),    
 ]
