50b402fc2fca6c68842e712aa2eb5d4970471543
Hemil Patel <167183225+Hemil77@users.noreply.github.com>
2026-06-04 14:41:30 +0530

    Clean up views by removing unused functions

--- score/views.py
+++ score/views.py
@@ -8,11 +8,3 @@ def homepage(request) :
 def pickleball(request) :
     return render(request, 'home.html')
 
-def om(request) :
-    return render(request, 'om.html')
-
-def energy(request) :
-    return render(request, 'energy.html')
-
-def vatsal(request) :
-    return render(request, 'vatsal.html')
